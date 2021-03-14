44a4570008a2e0fac20cf32733889159e42da0a7
xzx <xzxfirst@163.com>
2021-03-14 19:31:34 +0800

    feat: project init

---
+++ src/pages/index/index.tsx
@@ -0,0 +1,42 @@
+import { Component } from 'react'
+import { View, Text } from '@tarojs/components'
+import { AtButton,AtTabBar } from 'taro-ui'
+
+import "taro-ui/dist/style/components/button.scss" // 按需引入
+import './index.less'
+
+export default class Index extends Component {
+
+  componentWillMount () { }
+
+  componentDidMount () { }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  render () {
+    return (
+      <View className='index'>
+        <Text>Hello world!</Text>
+        <AtButton type='primary'>I need Taro UI</AtButton>
+        <Text>Taro UI 支持 Vue 了吗？</Text>
+        <AtButton type='primary' circle={true}>支持</AtButton>
+        <Text>共建？</Text>
+        <AtButton type='secondary' circle={true}>来</AtButton>
+        {/* <AtTabBar
+          fixed
+          selectedColor="#d43c33"
+          tabList={[
+            { title: "发现", iconPrefixClass: "fa", iconType: "feed" },
+            { title: "我的", iconPrefixClass: "fa", iconType: "music" }
+          ]}
+          onClick={this.switchTab.bind(this)}
+          current={this.state.current}
+        /> */}
+      </View>
+    )
+  }
+}
