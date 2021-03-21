a9a6f0a3fbac371a44ba66084f8d31d360b64060
AT <734237292@qq.com>
2021-03-22 07:16:41 +0800

    chore(package.json eslint ..): eslint 配置
    配置 eslint-plugin-taro
    tsconfig
    settings
    prettier

---
+++ .vscode/REDEME.md
@@ -0,0 +1,3 @@
+## 格式化文档
+
+安装 publisher:"Prettier" 插件用于格式化项目
---
+++ global.d.ts
@@ -0,0 +1,20 @@
+declare module '*.png'
+declare module '*.gif'
+declare module '*.jpg'
+declare module '*.jpeg'
+declare module '*.svg'
+declare module '*.css'
+// declare module '*.scss';
+
+declare module '*.module.less' {
+  const classes: { [key: string]: string }
+  export default classes
+}
+
+// @ts-ignore
+declare const process: {
+  env: {
+    TARO_ENV: 'weapp' | 'swan' | 'alipay' | 'h5' | 'rn' | 'tt' | 'quickapp' | 'qq'
+    [key: string]: any
+  }
+}
