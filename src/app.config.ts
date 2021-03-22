2536bd4865cf2046e84332e84c263be16b2d9cfc
AT <734237292@qq.com>
2021-03-22 08:50:38 +0800

    style(app.config.js/.cz-config.js): 格式化及修改pages页面配置
    less to scss

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,26 +1,29 @@
 export default {
-  pages: ["pages/publish/publish", 'pages/index/index', "pages/mine/mine"],
+  pages: ['pages/index/index', 'pages/publish/publish', 'pages/mine/mine'],
   tabBar: {
-    list: [{
-      // 'iconPath': 'resource/latest.png',
-      // 'selectedIconPath': 'resource/lastest_on.png',
-      pagePath: 'pages/index/index',
-      text: '动态'
-    }, {
-      // 'iconPath': 'resource/hotest.png',
-      // 'selectedIconPath': 'resource/hotest_on.png',
-      pagePath: 'pages/mine/mine',
-      text: '我的'
-    }],
-    'color': '#000',
-    'selectedColor': '#56abe4',
-    'backgroundColor': '#fff',
-    'borderStyle': 'black'
+    list: [
+      {
+        // 'iconPath': 'resource/latest.png',
+        // 'selectedIconPath': 'resource/lastest_on.png',
+        pagePath: 'pages/index/index',
+        text: '动态',
+      },
+      {
+        // 'iconPath': 'resource/hotest.png',
+        // 'selectedIconPath': 'resource/hotest_on.png',
+        pagePath: 'pages/mine/mine',
+        text: '我的',
+      },
+    ],
+    color: '#000',
+    selectedColor: '#56abe4',
+    backgroundColor: '#fff',
+    borderStyle: 'black',
   },
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
     navigationBarTitleText: 'WeChat',
-    navigationBarTextStyle: 'black'
-  }
-};
+    navigationBarTextStyle: 'black',
+  },
+}
