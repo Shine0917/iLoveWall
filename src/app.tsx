f3280e7245bbf1f8d1e6e26a09d832dbb8381219
AT <734237292@qq.com>
2021-04-12 11:08:08 +0800

    feat(app.tsx): 初始化云开发环境

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Taro from '@tarojs/taro'
 import { Provider } from 'react-redux'
 import dva from './utils/dva'
 import models from './models'
@@ -12,13 +13,24 @@ const dvaApp = dva.createApp({
 const store = dvaApp.getStore()
 
 class App extends Component {
-  componentDidMount() {}
+  componentDidMount() {
+    this.envInit()
+  }
 
   componentDidShow() {}
 
   componentDidHide() {}
 
   componentDidCatchError() {}
+
+  envInit = () => {
+    const weappEnv = Taro.getEnv() === Taro.ENV_TYPE.WEAPP
+    if (weappEnv) {
+      // 初始化云开发环境
+      const env = process.env.NODE_ENV === 'development' ? 'dev-1gboiotta79671d6' : 'prod-c0yn7'
+      Taro.cloud.init({ env, traceUser: true })
+    }
+  }
 
   render() {
     const { children } = this.props
